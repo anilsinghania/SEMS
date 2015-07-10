54a93cc90e4923131c64374a88acbfab5ec9399e
root <root@bss401dt036.betasoftsystems.com>
2015-07-11 03:11:14 +0530

    Add repo

---
+++ platforms/android/assets/www/js/vars.js
@@ -0,0 +1,11 @@
+var address="http://Rilsmartmeter.in/";
+var pictureSource;
+var destinationType;
+/*$(function(){
+	$("body").width($(window).width());
+	$("body").css("padding","0px!important");
+	$("body").css("margin","0px!important");
+	$("body").css("overflow-x","hidden!important")
+	$(".col-md-12.column").css("padding","0px!important");//column
+});*/
+	
